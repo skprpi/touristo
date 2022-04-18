d4dfb88ad5841111e26a5a235009abbbb31bffa4
Max <1@gmail.com>
2022-04-19 00:05:58 +0300

    feat: Add migrations

--- app/api/common/credentials.py
+++ app/api/common/credentials.py
@@ -16,7 +16,7 @@ class CredentialsDB:
     def get_database_url(cls):
         # return f'postgresql+psycopg2://{cls.POSTGRES_USER}:{cls.POSTGRES_PASSWORD}' \
             # f'@{cls.POSTGRES_SERVER}:{cls.POSTGRES_PORT}/{cls.POSTGRES_DB}'
-        return 'postgres://tnqrjszpbdwfzo:59deba175f8192f1c274efda57a62ec4f9f7d656078bf69d1c8bc4d640267414@ec2-99-80-170-190.eu-west-1.compute.amazonaws.com:5432/d6map5821v2dop'
+        return 'postgresql+psycopg2://tnqrjszpbdwfzo:59deba175f8192f1c274efda57a62ec4f9f7d656078bf69d1c8bc4d640267414@ec2-99-80-170-190.eu-west-1.compute.amazonaws.com:5432/d6map5821v2dop'
 
 
 class CredentialsJWT:
