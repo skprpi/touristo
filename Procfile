52e9e71b129542fc9f5b9c9d7fc9c074a6dd4a66
Max <1@gmail.com>
2022-04-22 19:58:47 +0300

    feat: Update post api

--- Procfile
+++ Procfile
@@ -1,2 +1,1 @@
-web: alembic upgrade head
 web: uvicorn app.main:fastapi_app --host 0.0.0.0 --port=${PORT:-5000}
--- app/api/posts/router.py
+++ app/api/posts/router.py
@@ -13,11 +13,17 @@ post_router = APIRouter(
 
 
 @post_router.post('')
-def create_post(post: schemas.CreatePost, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
-    new_post = Post(**post.dict())
+def create_post(request: schemas.CreatePost, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    new_post = Post(**request.dict())
     db.add(new_post)
     db.commit()
     db.refresh(new_post)
     return new_post
 
 
+@post_router.post('/{post_id}', response_model=schemas.Post)
+def get_post(post_id: int, db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    post = db.query(Post).filter(Post.id == post_id).first()
+    if not post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'No posts with id {post_id}')
+    return post
