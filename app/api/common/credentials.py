360279114a038417efffac5f7c0fdbab95b336f4
Maxim <fatin.maksim9067@gmail.com>
2022-06-07 19:05:24 +0300

    feat: Change db Postgres to sqlite

--- app/api/common/credentials.py
+++ app/api/common/credentials.py
@@ -6,21 +6,6 @@ env_path = os.path.join(os.path.dirname(__file__), '.env')
 load_dotenv(dotenv_path=env_path)
 
 
-class CredentialsDB:
-    POSTGRES_USER: str = os.getenv('POSTGRES_USER')
-    POSTGRES_PASSWORD: str = os.getenv('POSTGRES_PASSWORD')
-    POSTGRES_SERVER: str = os.getenv('POSTGRES_SERVER')
-    POSTGRES_PORT: str = os.getenv('POSTGRES_PORT')
-    POSTGRES_DB: str = os.getenv('POSTGRES_DB')
-    HEROKU_URL = 'jtlvyhctfevcwz:d51f070f3eeb305dce27536c45395896e6ed9a0d85e229c41005745824af841a \
-                  @ec2-99-81-137-11.eu-west-1.compute.amazonaws.com:5432/d6d6d1leanpbnn'
-
-    @classmethod
-    def get_database_url(cls):
-        return f'postgresql+psycopg2://{cls.POSTGRES_USER}:{cls.POSTGRES_PASSWORD}' \
-            f'@{cls.POSTGRES_SERVER}:{cls.POSTGRES_PORT}/{cls.POSTGRES_DB}'
-
-
 class CredentialsJWT:
     JWT_SECRET: str = os.getenv('JWT_SECRET')
     JWT_ALGORITHM: str = os.getenv('JWT_ALGORITHM')
--- app/main.py
+++ app/main.py
@@ -2,7 +2,9 @@ from .api.posts.router import post_router
 from .api.users.router import user_router, login_router
 from .api.locations.router import location_router
 from fastapi import FastAPI
+from .api.common.db import Base, engine
 
+Base.metadata.create_all(bind=engine)
 
 fastapi_app = FastAPI()
 fastapi_app.include_router(post_router)
