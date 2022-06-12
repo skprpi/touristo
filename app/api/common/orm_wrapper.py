d87b526b9cfa1f12128bc6711b8a07ffc2e5026e
Maxim <fatin.maksim9067@gmail.com>
2022-06-12 22:20:05 +0300

    feat: Add try except wrappers for sql query

--- app/api/common/orm_wrapper.py
+++ app/api/common/orm_wrapper.py
@@ -15,17 +15,23 @@ class SQLAlchemyWrap:
         return instance
 
     def create(self, request_field_dict: dict, db, current_user: (CurrentUser | None)):
-        new_instance = self.model_class(**request_field_dict)
-        db.add(new_instance)
-        db.commit()
-        db.refresh(new_instance)
+        try:
+            new_instance = self.model_class(**request_field_dict)
+            db.add(new_instance)
+            db.commit()
+            db.refresh(new_instance)
+        except Exception:
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail='Can not create database instance')
         return new_instance
 
     def partial_update_by_id(self, id: int, request_field_dict: dict, db: Session, current_user: (CurrentUser | None)):
-        instance = self.get_by_id(id, db, current_user)
-        for var, value in request_field_dict.items():
-            setattr(instance, var, value) if value else None
-        db.add(instance)
-        db.commit()
-        db.refresh(instance)
+        try:
+            instance = self.get_by_id(id, db, current_user)
+            for var, value in request_field_dict.items():
+                setattr(instance, var, value) if value else None
+            db.add(instance)
+            db.commit()
+            db.refresh(instance)
+        except Exception:
+            raise HTTPException(status_code=status.HTTP_409_CONFLICT, detail='Can not update database instance')
         return instance
